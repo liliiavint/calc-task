e0def30de8957fb0ed074a0ced527a2ce336fe15
liliia_vintiuk <rudapanda2016@gmail.com>
2024-03-29 16:47:28 +0200

    up

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const klaidaDOM = document.querySelector(".klaida");
 const resulttipDOM = document.querySelector(".resulttip");
 const resulttotalDOM = document.querySelector(".resulttotal");
 const btnscaicioDOM = document.querySelector(".btnscaicio");
-const rate1DOM = document.getElementById("15");
-const rate2DOM = document.getElementById("25");
+
 
 function count() {
 
     const bill = parseFloat(billDOM.value);
     const ratenumber = parseFloat(ratenumberDOM.value);
+    console.log(ratenumber)
     let result5 = (bill * 0.05).toFixed(2);
     let result15 = (bill * 0.15).toFixed(2);
     let result25 = (bill * 0.25).toFixed(2);
@@ -25,7 +25,7 @@ function count() {
         } else if (ratenumber === 2) {
             resulttipDOM.textContent = `$ ${result15}`;
             resulttotalDOM.textContent = `$ ${(bill + parseFloat(result15)).toFixed(2)}`;
-        }if (ratenumber === 3) {
+        } else if(ratenumber === 3) {
             resulttipDOM.textContent = `$ ${result25}`;
             resulttotalDOM.textContent = `$ ${(bill + parseFloat(result25)).toFixed(2)}`;
         }
